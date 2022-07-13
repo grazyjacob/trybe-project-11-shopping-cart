45f969b95e475d4713812de3d94f2ad2e555d7cb
Graziely Jacob <jacobgrazy@gmail.com>
2022-07-13 13:12:48 -0300

    Requisito 3 concluido

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,9 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (argumento) => {
+  if(!argumento) return new Error('You must provide an url')
+  const url = `https://api.mercadolibre.com/items/${argumento}`
+  const results = fetch(url)
+  .then((response) => response.json());
+  return results;
 };
 
 if (typeof module !== 'undefined') {
@@ -7,3 +11,5 @@ if (typeof module !== 'undefined') {
     fetchItem,
   };
 }
+
+
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,33 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se fetchItem é uma função', () => {
+    expect(typeof(fetchItem)).toEqual('function');
+  })
+
+  it('Se a função fetchItem for executada chama a fetch', () => {
+    fetchItem('MLB1615760527');
+    expect.assertions(1)
+    expect(fetch).toHaveBeenCalledTimes(1);
+  })
+
+  it('Se a função fetchItem for chamada sem nenhum parametro retorna um erro' , async () => {
+    const expected = new Error('You must provide an url')
+    expect(await fetchItem()).toEqual(expected);
+  })
+  it('Se chamar a função passando um argumento a função retorna um objeto igual ao "item"', async () => {
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  })
+  it('Se chamar a função passando um argumento a função utiliza um endpoint com o argumento passado', async () => {
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1615760527'
+    await fetchItem('MLB1615760527');
+
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  })
 });
+
+// - Teste se, ao chamar a função `fetchItem` com o argumento 
+// do item "MLB1615760527", a função `fetch` utiliza o endpoint
+// "https://api.mercadolibre.com/items/MLB1615760527";
+ 
+// - Será avaliado se os testes implementados atingem no mínimo 50% da cobertura total e 100% da função `fetchItem`.
