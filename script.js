7eb8183697eb509a808bfc89dd433f66fdf364a6
Graziely Jacob <jacobgrazy@gmail.com>
2022-07-14 08:17:44 -0300

    I started requirement 5

--- script.js
+++ script.js
@@ -24,29 +24,44 @@ const createProductItemElement = ({ sku, name, image }) => {
   return section;
 };
 
-// const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+const cartItems = document.querySelector('.cart__items');
 
-// const cartItemClickListener = (event) => {
-//   // coloque seu código aqui
-// };
+const cartItemClickListener = (event) => {
+  const click = event;
+  click.target.innerText = ''; 
+};
 
-// const createCartItemElement = ({ sku, name, salePrice }) => {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// };
+const createCartItemElement = ({ sku, name, salePrice }) => {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+};
+const addToCart = async (button) => { 
+  const parent = button.target.parentNode;
+  const itemID = getSkuFromProductItem(parent);
+  const promisse = await fetchItem(itemID);
+  const { id: sku, title: name, price: salePrice } = promisse;
+  cartItems.appendChild(createCartItemElement({ sku, name, salePrice }));
+ };
+
 const secaoItens = document.querySelector('.items');
 
 async function listaProdutos() {
+const getClassButton = document.getElementsByClassName('item__add');
 const { results } = await fetchProducts('computador');
 results.forEach(({ id: sku, title: name, thumbnail: image }) => {
     secaoItens.appendChild(createProductItemElement({ sku, name, image }));
 });
+[...getClassButton].forEach((button) => { 
+  button.addEventListener('click', addToCart);
+});
 }
-
 listaProdutos();
 
 window.onload = () => {  
 };
+
+// cartItems.innerHTML = '' // Limpa todos da lista
